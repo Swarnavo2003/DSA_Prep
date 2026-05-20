7e1eaaebe7c39c804477c4e81ea82b70bf3b90ef
Swarnavo2003 <swarnavo2020@gmail.com>
2026-05-20 18:26:59 +0530

    binary search added

---
+++ src/SearchingAndSorting/BinarySearch.java
@@ -0,0 +1,36 @@
+package SearchingAndSorting;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static int binarySearch(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i ++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+        int index = binarySearch(nums, target);
+        System.out.println(index);
+
+        sc.close();
+    }
+}

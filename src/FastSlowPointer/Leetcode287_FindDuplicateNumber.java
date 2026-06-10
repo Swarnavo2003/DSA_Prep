b4118b2e1baa6a0f5245efc8c3396ea130548036
Swarnavo2003 <swarnavo2020@gmail.com>
2026-06-10 16:40:47 +0530

    leetcode 287 find duplicate number space optimized

--- src/FastSlowPointer/Leetcode287_FindDuplicateNumber.java
+++ src/FastSlowPointer/Leetcode287_FindDuplicateNumber.java
@@ -6,16 +6,30 @@ import java.util.Set;
 
 public class Leetcode287_FindDuplicateNumber {
     public static int findDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+//        Set<Integer> set = new HashSet<>();
+//
+//        for(int num : nums) {
+//            if(set.contains(num)) {
+//                return num;
+//            }
+//            set.add(num);
+//        }
+//
+//        return -1;
 
-        for(int num : nums) {
-            if(set.contains(num)) {
-                return num;
-            }
-            set.add(num);
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = 0;
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return -1;
+        return slow;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
